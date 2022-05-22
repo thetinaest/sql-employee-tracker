7fe3222209586beec6e0216bed378882abb6e8df
LittleToes <kristina.r.brennan@gmail.com>
2022-05-21 22:45:58 -0500

    added in most of the functionality, updated db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const PORT = 3001;
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 
 const connection = mysql.createConnection({
@@ -26,28 +27,90 @@ const questions = [{
     ]
 }];
 
+const deptQuestion = [{
+    type: 'input',
+    name: 'deptAdd',
+    message: 'What department would you like to add?'
+}];
+
+const roleQuestions = [
+    {
+        type: 'input',
+        name: 'roleTitle',
+        message: 'What is the title of this role?'
+    },
+    {
+        type: 'number',
+        name: 'roleSalary',
+        message: 'What is the annual salary for this role? (Please only enter a number. Do not include commas or a currency symbol.'
+    },
+    {
+        type: 'list',
+        name: 'deptId',
+        message: 'What department does this role belong to?',
+        choices: [
+            'Engineering',
+            'Finance',
+            'Marketing'
+        ]
+    }
+];
+
+const employeeQuestions = [
+    {
+        type: 'input',
+        name: 'firstName',
+        message: 'What is the employee\'s FIRST name?'
+    },
+    {
+        type: 'input',
+        name: 'lastName',
+        message: 'What is the employee\'s LAST name?'
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What is this employee\'s role?',
+        choices: [
+            'Developer',
+            'Software Engineer',
+            'Accountant',
+            'Intern',
+            'Copywriter'
+        ]
+    },
+    {
+        type: 'list',
+        name: 'manager',
+        message: 'Who is this employee\'s manager?',
+        choices: [
+            'Mike Chan',
+            'Ashley Rodriquez'
+        ]
+    }
+]
+
 function init() {
     inquirer.prompt(questions)
     .then(answers => {
         switch(answers.firstChoices) {
             case 'View all departments':
                 viewAllDepts();
-                console.log('aye');
                 break;
             case 'View all roles':
-                console.log('yo');
+                viewAllRoles();
                 break;
             case 'View all employees':
-                console.log('3');
+                viewAllEmployees();
                 break;
             case 'Add a department':
-                console.log('4');
+                addDepartment();
                 break;
             case 'Add a role':
-                console.log('5');
+                addRole();
                 break;
             case 'Add an employee':
-                console.log('6');
+                addEmployee();
                 break;
             case 'Update employee role':
                 console.log('7');
@@ -57,9 +120,10 @@ function init() {
         };
     });
 };
+
 // TODO: don't forget to fomat all this data to look nice when it console logs
 function viewAllDepts() {
-    const statement = `SELECT * from departments;`;
+    const statement = `SELECT * FROM departments;`;
     connection.query(statement, (err, res) => {
         if (err) {
             console.log(err);
@@ -69,6 +133,138 @@ function viewAllDepts() {
         console.log(res);
         init();
     });
+};
+
+function viewAllRoles() {
+    const statement = `SELECT roles.id, title, salary, department_name FROM roles INNER JOIN departments ON roles.department_id = departments.id;`;
+    connection.query(statement, (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        console.log(res);
+        init();
+    });
+};
+
+function viewAllEmployees() {
+    const statement = `SELECT * FROM employees INNER JOIN roles ON employees.role_id = roles.id;`;
+    connection.query(statement, (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        //TODO: need to find out how to say which manager they report to based on manager id
+        console.log(res);
+        init();
+    });
+};
+
+function addDepartment() {
+    inquirer.prompt(deptQuestion)
+    .then(answer => {
+        const statement = `INSERT INTO departments (department_name) VALUES (?)`;
+        params = [answer.deptAdd];
+
+        connection.query(statement, params, (err, res) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('New department added!');
+            init();
+        });
+    })
+};
+
+function addRole() {
+    inquirer.prompt(roleQuestions)
+    .then(answer => {
+        console.log(answer);
+        const statement = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`;
+        params = [answer.roleTitle, answer.roleSalary, getDeptId(answer.deptId)];
+
+        connection.query(statement, params, (err, res) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('New role added!');
+            init();
+        });
+    })
+};
+
+function addEmployee() {
+    inquirer.prompt(employeeQuestions)
+    .then(answer => {
+        console.log(answer);
+        const statement = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+        params = [answer.firstName, answer.lastName, roleId(answer.role), managerId(answer.manager)];
+
+        connection.query(statement, params, (err, res) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('New employee added!');
+            init();
+        });
+    })
+};
+
+
+//functions that return ids that the db understands
+function getDeptId(deptId) {
+    let id = 0;
+    switch(deptId) {
+        case 'Developer':
+            id = 2;
+            break;
+        case 'Finance':
+            id = 3;
+            break;
+        case 'Marketing':
+            id = 4;
+            break;       
+    }
+    return id;
+}
+
+function roleId(role) {
+    let id = 0;
+    switch(role) {
+        case 'Engineering':
+            id = 2;
+            break;
+        case 'Software Engineer':
+            id = 3;
+            break;
+        case 'Accountant':
+            id = 4;
+            break; 
+        case 'Intern':
+            id = 5;
+            break;  
+        case 'Copywriter':
+            id = 6;
+            break;    
+    }
+    return id;
+}
+
+function managerId(manager) {
+    let id = 0;
+    switch(manager) {
+        case 'Mike Chan':
+            id = 1;
+            break;
+        case 'Ashley Rodriquez':
+            id = 2;
+            break; 
+    }
+    return id;
 }
 
 init();
